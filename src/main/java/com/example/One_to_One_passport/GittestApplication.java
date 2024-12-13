000b2268fbbf0d868c82f598b21234ee54e8f6c5
Keerthana <makeerthu29@gmail.com>
2024-12-13 10:23:48 +0530

    commit
    commit

---
+++ src/main/java/com/example/One_to_One_passport/GittestApplication.java
@@ -0,0 +1,13 @@
+package com.example.One_to_One_passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/One_to_One_passport/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.One_to_One_passport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
